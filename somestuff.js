3be5a6bda6023a77065ceddc39dea402554bf278
Mancilla <mancilaz@dca9048571ee.ant.amazon.com>
2019-07-10 14:22:08 -0700

    Initial commit

---
+++ somestuff.js
@@ -0,0 +1,11 @@
+function myFunction(){
+  alert("whyd you click?")
+}
+
+function overimage(){
+  show("A Picture of a Bearded Dragon.")
+}
+
+function replaceImage(){
+  document.getElementById("cats").src="https://cf-s3.petcoach.co/thumbnails/article/uploads/articles/6316/30029c696ccf16f0148cf3a70e45c45daeb6561f.jpeg"
+}
